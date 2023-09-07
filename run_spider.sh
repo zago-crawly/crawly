d8a3c039b1fb30d789becb851ca99fc3effbac1f
zippy-junior <alexey@zago.ru>
2023-09-07 18:05:16 +0300

    #13 Removed unnessacery files

--- run_spider.sh
+++
@@ -1,7 +0,0 @@
-docker-compose \
--f docker/compose/docker-compose.scheduler.yml \
--f docker/compose/docker-compose.rabbitmq.yml \
--f docker/compose/docker-compose.mongodb.yml \
--f docker/compose/docker-compose.nginx.yml \
--f docker/compose/docker-compose.spider.yml \
-up --build -d
--- run_template_storage.sh
+++
@@ -1,6 +0,0 @@
-docker-compose \
--f docker/compose/docker-compose.nginx.yml \
--f docker/compose/docker-compose.postgres.yml \
--f docker/compose/docker-compose.template_storage.yml \
--f docker/compose/docker-compose.rabbitmq.yml \
-up --build -d
