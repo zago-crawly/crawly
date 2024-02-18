b206b43606947a8cb95ca665ecd7262cf461510c
zippy-junior <alexey@zago.ru>
2024-02-18 23:36:57 +0300

    #64 Realized

--- docker/microservice/run.sh
+++ docker/microservice/run.sh
@@ -1,12 +1,7 @@
 docker-compose \
--f docker/compose/docker-compose.scheduler.yml \
 -f docker/compose/docker-compose.rabbitmq.yml \
--f docker/compose/docker-compose.mongodb.yml \
 -f docker/compose/docker-compose.postgres.yml \
 -f docker/compose/docker-compose.schema_storage.yml \
 -f docker/compose/docker-compose.template_storage.yml \
 -f docker/compose/docker-compose.signal.yml \
--f docker/compose/docker-compose.nginx.yml \
--f docker/compose/docker-compose.spider.yml \
--f docker/compose/docker-compose.item_storage.yml \
 up --build -d
